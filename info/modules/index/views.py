2b720306e958e27385a347190c53224b095b6112
信洪贺 <1820233206@qq.com>
2019-06-10 15:35:57 +0800

    添加测试数据,静态文件,添加跟路由和路标路由

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -5,6 +5,8 @@
 # 创建蓝图对象
 from info.modules.index import index_blu
 # import logging
+from flask import render_template
+from flask import current_app
 
 
 
@@ -16,5 +18,11 @@ def index():
     # logging.error('测试error')
     # logging.fatal('测试fatal')
 
-    return "index"
+    return render_template("news/index.html")
+
+# TODO 在打开网页的时候，浏览器会默认去请求根路径和图标函数
+@index_blu.route('/favicon.ico')
+def set_favicon():
+    # send_static_file　是flask 去查找指定的静态文件所调用的方法
+    return current_app.send_static_file('news/favicon.ico')
 
